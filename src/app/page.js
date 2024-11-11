29999fcd7f03489ee7f9dd5bc7d8857f88059009
Nishit Lugun <lugun.nishit@gmail.com>
2024-11-12 03:30:00 +0530

    corrected loading

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import { Suspense, lazy, useEffect } from "react";
 import LocomotiveScroll from "locomotive-scroll";
+import Loading from "./components/Loading";
 
 // Lazy-load the components
 const Page1 = lazy(() => import("./components/Page1"));
@@ -12,62 +13,6 @@ const Page4 = lazy(() => import("./components/Page4"));
 const Page5 = lazy(() => import("./components/Page5"));
 const Page6 = lazy(() => import("./components/Page6"));
 const Page7 = lazy(() => import("./components/Page7"));
-
-const Loading = () => {
-  return (
-    <div className="h-screen w-full flex flex-col items-center justify-center z-[100] bg-black">
-      <h2 className="text-xl text-white font-semibold mb-5">
-        Loading FunToy World...
-      </h2>
-      <div className="loader-container">
-        <div className="loader"></div>
-      </div>
-
-      <style jsx>{`
-        .loader-container {
-          position: relative;
-          width: 150px;
-          height: 150px;
-        }
-
-        .loader {
-          position: absolute;
-          width: 100%;
-          height: 100%;
-          border: 16px solid transparent;
-          border-radius: 50%;
-          border-top: 16px solid #3498db;
-          animation: rotateLoader 1.5s ease-in-out infinite,
-            pulseLoader 1.5s infinite;
-        }
-
-        @keyframes rotateLoader {
-          0% {
-            transform: rotate(0deg);
-          }
-          100% {
-            transform: rotate(360deg);
-          }
-        }
-
-        @keyframes pulseLoader {
-          0% {
-            transform: scale(0.9);
-            opacity: 1;
-          }
-          50% {
-            transform: scale(1);
-            opacity: 0.7;
-          }
-          100% {
-            transform: scale(0.9);
-            opacity: 1;
-          }
-        }
-      `}</style>
-    </div>
-  );
-};
 
 const Page = () => {
   useEffect(() => {
